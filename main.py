5854e7081ae2fc3aa9cde5a585162420af25c048
natha <nathanm1034@tamu.edu>
2023-07-27 12:21:18 -0500

    Bot can interact with database. Created test commands for db interaction.

--- main.py
+++ main.py
@@ -1,19 +1,88 @@
 import discord
+import mysql.connector
 import os
-# from discord.ext import commands
-from dotenv import load_dotenv
+from discord import option
+from dotenv import load_dotenv # for local hosting
 
+bank = mysql.connector.connect(
+    host="212.192.29.151",
+    user="u102097_5FSL20P3iS",
+    password="PoE12YLUwKBYj4ZCnh5@@UVy",
+    database="s102097_Bank"
+)
 
-bot = discord.Bot()
+bankCursor = bank.cursor(dictionary=True)
+
 guild = [1131313562162319481]
-load_dotenv()
+bot = discord.Bot(guild_ids=guild)
+load_dotenv() # for local hosting
+
+db = bot.create_group(name="db", guild_ids=guild)
 
 @bot.event
 async def on_ready():
-    print(f"We have logged in as {bot.user}")  
+    print(f"We have logged in as {bot.user}")   
+  
+@db.command(description="Create Account")
+async def create(ctx):
+    userID = ctx.author.id
+    bankCursor.execute(f"SELECT * FROM `Bank Test` WHERE ID = {userID}")
+    user = bankCursor.fetchone()
     
-@bot.slash_command(name="ping", description="Test Ping", guild_ids=guild)
-async def ping2(ctx):
-    await ctx.respond("Pong!")
-        
+    if user is None:
+        query = f"INSERT INTO `Bank Test` (ID, Balance) VALUES ({userID}, 0)"
+        bankCursor.execute(query)
+        bank.commit()
+        await ctx.respond("Account created")
+    else:
+        await ctx.respond("Account already exist")
+    
+    
+@db.command(description="Add Money")
+@option(
+    "amount",
+    description="Enter balance",
+    required=False,
+    default=500
+)
+async def insert(ctx, amount: int):
+    userID = ctx.author.id
+    bankCursor.execute(f"SELECT * FROM `Bank Test` WHERE ID = {userID}")
+    user = bankCursor.fetchone()
+    
+    if user is not None:
+        newBalance = user['Balance'] + amount
+        query = f"UPDATE `Bank Test` SET Balance = {newBalance} WHERE ID = {userID}"
+        bankCursor.execute(query)
+        bank.commit()
+        await ctx.respond(f"Your new balance is {newBalance}")
+    else:
+        await ctx.respond(f"Create an account first")
+    
+@db.command()
+async def fetch(ctx):
+    userID = ctx.author.id
+    bankCursor.execute(f"SELECT * FROM `Bank Test` WHERE ID = {userID}")
+    user = bankCursor.fetchone()
+    
+    if user is not None:
+        currentBalance = user['Balance']
+        await ctx.respond(f"Your current balance is {currentBalance}")
+    else:
+        await ctx.respond(f"Create an account first")
+
+@db.command()
+async def delete(ctx):
+    userID = ctx.author.id
+    bankCursor.execute(f"SELECT * FROM `Bank Test` WHERE ID = {userID}")
+    user = bankCursor.fetchone()
+    
+    if user is not None:
+        query = f"DELETE FROM `Bank Test` WHERE ID = {userID}"
+        bankCursor.execute(query)
+        bank.commit()
+        await ctx.respond("Account deleted")
+    else:
+        await ctx.respond(f"Nothing to delete")
+
 bot.run(os.getenv('TOKEN'))
